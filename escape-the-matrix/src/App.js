90f7c1c577e8de52867739f5727f60e20e015974
AlexanderWeinberg <alexw.dagreat@gmail.com>
2020-11-28 17:47:54 -0800

    basic firebase API incorporated with a login pop up

--- escape-the-matrix/src/App.js
+++ escape-the-matrix/src/App.js
@@ -1,13 +1,21 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from "./login_Button.js";
 
 function App() {
+  const [user, setUser] = useState(null) //setUser is declared
   return (
     <div className="App">
       <header className="App-header">
+
+        {/* Login Button and login success message */}
+        <LoginButton setUser={(user) => setUser(user)} />
+        {user != null && <p> Welcome to the Matrix, {user.displayName}</p>}
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Escape the Matrix
         </p>
         <a
           className="App-link"
